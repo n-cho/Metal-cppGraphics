69f8bcb05b72f18452cdca501954c6972375a64e
Nathan Cho <nathanjwc06@gmail.com>
2024-05-11 11:15:15 -0700

    init commit

---
+++ build/CMakeFiles/Graphics.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Graphics.dir/main.cpp.o"
+  "CMakeFiles/Graphics.dir/main.cpp.o.d"
+  "Graphics"
+  "Graphics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Graphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
